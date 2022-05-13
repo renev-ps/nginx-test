3bbe1f583ddc9a9dd743def9080e27943fa4df02
rene <rene.vega@peerspace.com>
2022-05-13 19:07:28 -0400

    test 707

--- push.sh
+++ push.sh
@@ -12,3 +12,9 @@ echo "*****${COMMITID}********"
 
 docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${COMMITID}" \
 -t ${TAG} --push .
+
+echo "To run container run:"
+echo "docker run -d -p 9090:80 --name rene-nginx gcr.io/ps-prod-container-repo/rene-nginx:latest-${COMMITID}"
+echo
+echo "After running to remove container run:"
+echo "docker rm -f rene-nginx"
