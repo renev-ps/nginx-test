43c14d5c543cc53a81cd2db1c16827870a2aa5f0
rene <rene.vega@peerspace.com>
2022-05-13 18:51:40 -0400

    test 6451

--- push.sh
+++ push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 SERVICE="rene-nginx"
-VERSION=$(git log -1 --pretty=%h)
+COMMITID=$(git log -1 --pretty=%h)
 REPO="gcr.io/ps-prod-container-repo/"
-TAG="$REPO$SERVICE:${1}-$VERSION"
+TAG="$REPO$SERVICE:${1}-$COMMITID"
 echo ${TAG}
 
+echo "${COMMITID}" >> index.html
 
+echo "*****${COMMITID}********"
 
-echo "*****${VERSION}********"
-
-docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${VERSION}" \
+docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${COMMITID}" \
 -t ${TAG} --push .
