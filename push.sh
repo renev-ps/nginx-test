4d3b7dce27caadd965c076d55de27ebc0204df3d
rene <rene.vega@peerspace.com>
2022-05-13 18:42:57 -0400

    test 642

--- push.sh
+++ push.sh
@@ -6,6 +6,7 @@ REPO="gcr.io/ps-prod-container-repo/"
 TAG="$REPO$SERVICE:${1}-$VERSION"
 echo ${TAG}
 
+
 echo "*****${VERSION}********"
 
 docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${VERSION}" \
