b0424d8db0b2617015db3b79835d78fbb7f8c40f
rene <rene.vega@peerspace.com>
2022-05-13 18:27:32 -0400

    test

--- push.sh
+++ push.sh
@@ -6,10 +6,12 @@ REPO="gcr.io/ps-prod-container-repo/"
 TAG="$REPO$SERVICE:${1}-$VERSION"
 echo ${TAG}
 
+echo "*****${VERSION}********"
+
 if [[ -z "${1}" ]]; then
-  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="$VERSION" \
+  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${VERSION}" \
     -t ${TAG} --push .
 else
-  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="$VERSION" \
+  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="${VERSION}" \
     -t ${TAG} --push .
 fi
