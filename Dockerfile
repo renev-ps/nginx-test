65b9f072d43742bc80b52faeedf95062658819c7
rene <rene.vega@peerspace.com>
2022-05-13 18:17:33 -0400

    test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM nginx
 
+ARG $VERSION
+ENV VERSION=$VERSION
 COPY index.html /usr/share/nginx/html/
 
 
--- push.sh
+++ push.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-service=rene-nginx
+SERVICE="rene-nginx"
+VERSION=$(git log -1 --pretty=%h)
+REPO="gcr.io/ps-prod-container-repo/"
+TAG="$REPO$SERVICE:${1}-$VERSION"
+echo ${TAG}
 
 if [[ -z "${1}" ]]; then
-  docker buildx build --platform linux/arm64,linux/amd64 \
-    -t gcr.io/ps-prod-container-repo/${service} --push .
+  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="$VERSION" \
+    -t ${TAG} --push .
 else
-  docker buildx build --platform linux/arm64,linux/amd64 \
-    -t gcr.io/ps-prod-container-repo/${service}:"${1}" --push .
+  docker buildx build --platform linux/arm64,linux/amd64 --build-arg VERSION="$VERSION" \
+    -t ${TAG} --push .
 fi
